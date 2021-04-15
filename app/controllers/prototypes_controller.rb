30a44b02a34541937c51c6080bca66c756ef05b4
小沼智則 <omatomemail0718@gmail.com>
2021-04-15 18:22:58 +0900

    修正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,except: [:index,:show, :create,:new]
+  before_action :authenticate_user!,except: [:index,:show, :create,:new,:edit]
   # before_action :move_to_index, except: [:index, :show, :create]
   def index
     @users = User.all
